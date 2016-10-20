cdb8f53621f64524630086480d478cbb6ab6159e
TatsuLee <dli37@hawk.iit.edu>
2016-10-20 09:48:12 -0500

    l43 done

---
+++ l22.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+class Solution(object):
+
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
--- t.py
+++ t.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import l20
-b = ""
-test = l20.Solution()
-out = test.isValid(b)
-print out
+
+import l43
+
+a,b='123','233'
+print 'input:',[a,b]
+print 'true ans:', int(a)*int(b)
+test = l43.Solution()
+out = test.multiply(a,b)
+print 'output:',out
 
