561bdbc4f61d6df37b21f3932145065f9ce2ad42
TatsuLee <dli37@hawk.iit.edu>
2016-10-31 19:57:42 -0500

    l168,169 done

--- t.py
+++ t.py
@@ -2,10 +2,10 @@
 # -*- coding: utf-8 -*-
 
 
-import l166
-a, b = 4, 333 
-print 'input:', [a, b]
-print 'true ans:', a*1.0/b
-test = l166.Solution()
-out = test.fractionToDecimal(a, b)
+import l169
+a = 'ZZ' 
+print 'input:', a
+print 'true ans:', a
+test = l169.Solution()
+out = test.titleToNumber(a)
 print 'output:', out
